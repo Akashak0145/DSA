b0ee3bf4f8257042e2986dbf51766ef15442cee8
Akashak0145 <akashak0145@gmail.com>
2026-06-30 11:51:28 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (37.22%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public static void main (String[]args){
+        char[] s={'h','e','l','l','o'};
+        reverseString(s);
+        System.out.println(Arrays.toString(s));
+    }
+    public static void reverseString(char[] s) {
+        int l=0;
+        int r=s.length-1;
+        while(l<r){
+            char temp=s[l];
+            s[l]=s[r];
+            s[r]=temp;
+            l++;
+            r--;
+        }
+        
+    }
+}
