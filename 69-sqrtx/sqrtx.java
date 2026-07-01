ca483d002ecf91a2061821486bbf9206d6008c0c
Akashak0145 <akashak0145@gmail.com>
2026-07-01 11:36:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (61.19%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public static void main (String []args){
+        int x=4;
+        System.out.println(x);
+    }
+    public int mySqrt(int x) {
+      return (int) Math.sqrt(x);
+    }
+}
